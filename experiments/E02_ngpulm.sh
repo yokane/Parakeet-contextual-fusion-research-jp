e081b9e5743e6801e1a184439308a4b75ab0c2b3
yokane <42724351+yokane@users.noreply.github.com>
2026-09-01 07:44:01 +0900

    exp: add E02 NGPU-LM shallow fusion

---
+++ experiments/E02_ngpulm.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+source "$(dirname "$0")/_common.sh"
+require_file "${MANIFEST}"
+require_file "${NGPU_LM}"
+
+nemo_eval \
+  pretrained_name="${MODEL_NAME}" \
+  dataset_manifest="${MANIFEST}" \
+  output_filename="${RESULTS_DIR}/E02_ngpulm.json" \
+  batch_size="${BATCH_SIZE}" \
+  use_cer=true \
+  decoder_type=rnnt \
+  rnnt_decoding.strategy=malsd_batch \
+  rnnt_decoding.beam.beam_size="${BEAM_SIZE}" \
+  rnnt_decoding.beam.pruning_mode=late \
+  rnnt_decoding.beam.blank_lm_score_mode=lm_weighted_full \
+  rnnt_decoding.beam.allow_cuda_graphs=true \
+  rnnt_decoding.beam.ngram_lm_model="${NGPU_LM}" \
+  rnnt_decoding.beam.ngram_lm_alpha="${LM_ALPHA}"
